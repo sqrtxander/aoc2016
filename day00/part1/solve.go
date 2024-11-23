a11f2584c213424065a8a10079f79238de327186
sqrtxander <xanderjames128@gmail.com>
2024-11-24 10:38:44 +1100

    Preparation for aoc2024 in go by doing aoc2016

---
+++ day00/part1/solve.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func solve(s string) int {
+	lines := strings.Split(s, "\n")
+
+	for i, line := range lines {
+		_ = i
+		_ = line
+	}
+
+	fmt.Println(s)
+
+	return 0
+}
+
+func main() {
+	var inputPath string
+	if len(os.Args) > 1 {
+		inputPath = os.Args[1]
+	} else {
+		_, currentFilePath, _, _ := runtime.Caller(0)
+		dir := filepath.Dir(currentFilePath)
+		dir = filepath.Dir(dir)
+		inputPath = filepath.Join(dir, "input.in")
+	}
+	contents, err := os.ReadFile(inputPath)
+	if err != nil {
+		log.Fatalf("Error reading file %s:\n%v\n", inputPath, err)
+		return
+	}
+	fmt.Println(solve(string(contents)))
+}
---
+++ support/getinput/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"aoc2016/support"
+)
+
+func main() {
+    support.DownloadInput()
+}
